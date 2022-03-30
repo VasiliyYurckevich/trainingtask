6d1510ae8775758ce8c3cc4b2dbd14921a1def7c
VasiliyYurckevich <92052945+VasiliyYurckevich@users.noreply.github.com>
2022-03-30 12:46:30 +0300

    #14  Добавление/ редактирование проекта. Поле "Описание": ошибка при сохранении html тегов, определяющих html страницу

--- src/main/java/com/qulix/yurkevichvv/trainingtask/controller/EmployeeController.java
+++ src/main/java/com/qulix/yurkevichvv/trainingtask/controller/EmployeeController.java
@@ -18,13 +18,23 @@ import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-
+/**
+ * Controller for Employees
+ *
+ * @author YurkevichVV
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public class EmployeeController extends HttpServlet {
 
     private static final long serialVersionUID = 12345L;
     private DAOInterface<Employee> employeeInterface;
 
-    public static final Logger logger = Logger.getLogger(EmployeeController.class.getName());
+    public static final Logger logger = Logger.getLogger(EmployeeController.class.getName());//logger
+    /**
+     * Initialize the Employee servlet.
+     */
     public void init() throws ServletException {
         super.init();
 
@@ -34,19 +44,28 @@ public class EmployeeController extends HttpServlet {
             e.printStackTrace();
         }
     }
+    /**
+     * Processes requests for HTTP  POST method.
+     * methods.
+     *
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
 
         try {
-            String action = req.getParameter("action");
+            String action = req.getParameter("action");// get action from form
 
             switch (action) {
                 case "/add":
-                    addEmployee(req, resp);
+                    addEmployee(req, resp);//add employee
                     break;
                 case "/update":
-                    updateEmployee(req, resp);
+                    updateEmployee(req, resp);//update employee
                     break;
 
             }
@@ -54,30 +73,38 @@ public class EmployeeController extends HttpServlet {
             e.printStackTrace();
         }
     }
-
+    /**
+     * Processes requests for HTTP  GET method.
+     * methods.
+     *
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
 
         try {
-            String action = req.getParameter("action");
+            String action = req.getParameter("action");// get action from form
 
             if (action == null) {
-                action = "/list";
+                action = "/list";   // default action
             }
 
             switch (action) {
                 case "/list":
-                    listEmployees(req, resp);
+                    listEmployees(req, resp);// get list employees
                     break;
                 case "/delete":
-                    deleteEmployee(req, resp);
+                    deleteEmployee(req, resp);//delete employee
                     break;
                 case "/new":
-                    addEmployeeForm(req, resp);
+                    addEmployeeForm(req, resp);//open add employee form
                     break;
                 case "/edit":
-                    updateEmployeeForm(req, resp);
+                    updateEmployeeForm(req, resp);//open update employee form
             }
 
 
@@ -86,10 +113,17 @@ public class EmployeeController extends HttpServlet {
         }
     }
 
+    /**
+     * Method for open update employee form
+     *
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws Exception
+     */
     private void updateEmployeeForm(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException {
-        Integer employeeId = Integer.valueOf(req.getParameter("employeeId"));
-        Employee existingEmployee= employeeInterface.getById(employeeId);
-        req.setAttribute("employeeId",employeeId);
+        Integer employeeId = Integer.valueOf(req.getParameter("employeeId"));//get employee id from form
+        Employee existingEmployee= employeeInterface.getById(employeeId);//get employee from db
+        req.setAttribute("employeeId",employeeId);//set attributes for form
         req.setAttribute("surname", existingEmployee.getSurname());
         req.setAttribute("firstName", existingEmployee.getFirstName());
         req.setAttribute("lastName", existingEmployee.getLastName());
@@ -100,12 +134,24 @@ public class EmployeeController extends HttpServlet {
         dispatcher.forward(req, resp);
     }
 
+    /**
+     * Method for open add employee form
+     *
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws Exception
+     */
     private void addEmployeeForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher("/add-employee-form.jsp");
         dispatcher.forward(req, resp);
     }
 
-
+    /**
+     * Method for delete employee
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws Exception
+     */
     private void deleteEmployee(HttpServletRequest req, HttpServletResponse resp){
         try {
             Integer theEmployeeId = Integer.valueOf(req.getParameter("employeeId"));
@@ -118,6 +164,13 @@ public class EmployeeController extends HttpServlet {
 
     }
 
+    /**
+     * Method for update employee
+     *
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws Exception
+     */
     private void updateEmployee(HttpServletRequest req, HttpServletResponse resp)  {
         try {
             int employeeId = Integer.parseInt(req.getParameter("employeeId"));
@@ -135,7 +188,13 @@ public class EmployeeController extends HttpServlet {
     }
 
 
-
+    /**
+     * Method for add employee
+     *
+     * @param req servlet request
+     * @param resp servlet response
+     * @throws Exception
+     */
     private void addEmployee(HttpServletRequest req, HttpServletResponse resp) {
        try {
            String surname = req.getParameter("surname");
@@ -146,11 +205,20 @@ public class EmployeeController extends HttpServlet {
            employeeInterface.add(employee);
            listEmployees(req, resp);
            logger.info("Employee with id created");
-      }catch ( SQLException| ServletException | IOException ex){
+       }catch ( SQLException| ServletException | IOException ex){
            logger.log(Level.SEVERE, "Error message", ex);
-    }
+       }
     }
 
+    /**
+     * Method for get list employees
+     *
+     * @param req
+     * @param resp
+     * @throws SQLException
+     * @throws ServletException
+     * @throws IOException
+     */
     private void listEmployees(HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException {
         List<Employee> employees = employeeInterface.getAll();
         req.setCharacterEncoding("UTF-8");
