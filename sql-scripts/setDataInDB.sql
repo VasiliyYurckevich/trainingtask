a54985d33a9f861e50940b42f3d3045a6f48827e
yurkevichvv <yurkevichvv@qulix.com>
2022-06-27 15:15:24 +0300

    #101 Проблемы оформления в SQL-файлах

--- sql-scripts/setDataInDB.sql
+++ sql-scripts/setDataInDB.sql
@@ -1,43 +1,43 @@
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 1,'Сидоров', 'Евгений', 'Николаевич', 'Младший разработчик');
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 2, 'Зеленский', 'Владимир', 'Александрович', 'Продукт-менеджер');
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 3, 'Ельцин', 'Борис', 'Николаевич', 'Бизнес-аналитик');
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 4, 'Джугашвили', 'Иосиф', 'Виссарионович', 'Старший разработчик');
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 5, 'Сикорский', 'Игорь', 'Иванович', 'Разработчик');
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 6, 'Меньшиков', 'Александр', 'Данилович', 'Тестировщик');
-INSERT INTO "PUBLIC"."EMPLOYEE"( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 7, 'Котиков', 'Степан', 'Николаевич', 'Дизайнер');
-INSERT INTO "PUBLIC"."PROJECT" ( "ID", "TITLE", "DESCRIPTION" )
+INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" )
 VALUES ( 1, 'MobilBank', 'Мобильное банковское приложение');
-INSERT INTO "PUBLIC"."PROJECT" ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 2, 'Microsoft', 'Операционная система');
-INSERT INTO "PUBLIC"."PROJECT" ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 3, 'Witcher 4', 'Компьютерная игра');
-INSERT INTO "PUBLIC"."PROJECT" ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 4, 'PowerPoint', 'Программа для презентаций');
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 2, 'Microsoft', 'Операционная система');
+INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 3, 'Witcher 4', 'Компьютерная игра');
+INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 4, 'PowerPoint', 'Программа для презентаций');
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 1, 1, 'Составить отчет о экономической выгоде', 35, '2022-02-28', '2022-03-05', 3, 3);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 2, 2, 'Добавить функцию просмотра расходов клиента', 20, '2022-04-11', '2022-04-12', 5, 1);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 3, 2, 'Полное тестирование функциональности', 170, '2022-03-28', '2022-04-30', 6, 2);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 4, 4, '№132 Ошибка при вводе некорректных данных', 20, '2022-03-11', '2022-03-15', 5, 1);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 5, 2, 'Разработать дизайн диаграмм расходов', 2, '2022-03-28', '2022-03-29', 7, 1);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 6, 1, 'Доработать игровой движок', 235, '2022-03-28', '2022-05-05', 4, 3);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 7, 3, '№51 Неудачный выбор типа для атрибута Task.status', 8, '2022-03-11', '2022-03-12', 1, 1);
-INSERT INTO "PUBLIC"."TASK"
+INSERT INTO task
 ( "ID", "STATUS", "TITLE", "WORK_TIME", "BEGIN_DATE", "END_DATE", "EMPLOYEE_ID", "PROJECT_ID" )
 VALUES ( 8, 3, 'Составить отчет для клиента', 25, '2022-02-28', '2022-03-05', NULL, 3);
