9c19d807e7d1ac21c0037dd2c63053a6f4734c2a
yurkevichvv <yurkevichvv@qulix.com>
2022-06-27 15:34:13 +0300

    #101 Проблемы оформления в SQL-файлах

--- sql-scripts/setDataInDB.sql
+++ sql-scripts/setDataInDB.sql
@@ -1,19 +1,18 @@
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 1,'Сидоров', 'Евгений', 'Николаевич', 'Младший разработчик');
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 2, 'Зеленский', 'Владимир', 'Александрович', 'Продукт-менеджер');
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 3, 'Ельцин', 'Борис', 'Николаевич', 'Бизнес-аналитик');
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 4, 'Джугашвили', 'Иосиф', 'Виссарионович', 'Старший разработчик');
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 5, 'Сикорский', 'Игорь', 'Иванович', 'Разработчик');
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 6, 'Меньшиков', 'Александр', 'Данилович', 'Тестировщик');
-INSERT INTO employee( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
+INSERT INTO employee ( "ID", "SURNAME", "FIRST_NAME", "PATRONYMIC", "POST" )
 VALUES ( 7, 'Котиков', 'Степан', 'Николаевич', 'Дизайнер');
-INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" )
-VALUES ( 1, 'MobilBank', 'Мобильное банковское приложение');
+INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 1, 'MobilBank', 'Мобильное банковское приложение');
 INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 2, 'Microsoft', 'Операционная система');
 INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 3, 'Witcher 4', 'Компьютерная игра');
 INSERT INTO project ( "ID", "TITLE", "DESCRIPTION" ) VALUES ( 4, 'PowerPoint', 'Программа для презентаций');
