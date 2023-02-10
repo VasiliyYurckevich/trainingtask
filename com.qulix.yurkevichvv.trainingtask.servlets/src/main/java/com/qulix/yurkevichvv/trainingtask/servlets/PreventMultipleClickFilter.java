3e1dff8d6d99b460360bf5cff06201ed52a51994
yurkevichvv <yurkevichvv@qulix.com>
2023-02-10 17:41:10 +0300

    PreventMultipleClickFilter.java

---
+++ com.qulix.yurkevichvv.trainingtask.servlets/src/main/java/com/qulix/yurkevichvv/trainingtask/servlets/PreventMultipleClickFilter.java
@@ -0,0 +1,18 @@
+//package com.qulix.yurkevichvv.trainingtask.servlets;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import java.io.IOException;
+//import java.util.Set;
+//
+//public class PreventMultipleClickFilter implements Filter {
+//   // private Set<String> tokens
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        System.out.println(request.hashCode());
+//    }
+//}
