fba077740ee219a962c2bc09b9fb84f0f406fae2
yurkevichvv <yurkevichvv@qulix.com>
2023-02-16 17:23:42 +0300

    fix wicket app csp

--- com.qulix.yurkevichvv.trainingtask.wicket/src/main/java/com/qulix/yurkevichvv/trainingtask/wicket/TrainingTaskApplication.java
+++ com.qulix.yurkevichvv.trainingtask.wicket/src/main/java/com/qulix/yurkevichvv/trainingtask/wicket/TrainingTaskApplication.java
@@ -1,14 +1,18 @@
 package com.qulix.yurkevichvv.trainingtask.wicket;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.Session;
+import org.apache.wicket.csp.CSPDirective;
+import org.apache.wicket.csp.CSPDirectiveSrcValue;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
 
 import com.qulix.yurkevichvv.trainingtask.wicket.pages.base.HomePage;
+import org.apache.wicket.settings.RequestCycleSettings;
 
 /**
  * Базовый класс приложения.
@@ -25,12 +29,14 @@ public class TrainingTaskApplication extends WebApplication {
     @Override
     protected void init() {
         super.init();
-        getCspSettings().blocking().disabled();
+        getCspSettings().blocking().add(CSPDirective.STYLE_SRC, CSPDirectiveSrcValue.SELF);
+        getRequestCycleSettings().setRenderStrategy(RequestCycleSettings.RenderStrategy.REDIRECT_TO_RENDER);
     }
 
     @Override
     public Session newSession(Request request, Response response) {
-        return super.newSession(request, response).setLocale(new Locale("ru", "RU"));
+        return super.newSession(request, response).setLocale(new Locale("ru", "RU"))
+            .setAttribute("TOKEN_LIST", new ArrayList<String>());
     }
 }
 
